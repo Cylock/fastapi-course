84d891a11e42fe17c5ce4434825e08dfdaa857ae
Cylock <51211727+Cylock@users.noreply.github.com>
2023-12-26 18:04:02 +0000

    save progress

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+from random import randrange
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id: int):
+    for elem in my_posts:
+        if elem["id"] == id: 
+            return elem
+
 
 @app.get("/")
 def root():
@@ -10,8 +28,16 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    return  {"new_post:" f"title: {payload["title"]} content: {payload['content']}"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump() # dic() method is deprecated
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return  {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post_by_id(id):
+    post = find_post(int(id))
+    return post
