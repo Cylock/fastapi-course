6dcc26ee31adbb81bff283f0cdd309dc125721db
Cylock <51211727+Cylock@users.noreply.github.com>
2024-01-22 09:34:17 +0000

    Solving merge issues

---
+++ python-learning.session.sql
@@ -0,0 +1,12 @@
+-- CREATE DATABASE fastapi
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     CONNECTION LIMIT = -1;
+
+
+-- CREATE TABLE products (
+--     name VARCHAR NOT NULL,
+--     price INT NOT NULL,
+--     id SERIAL PRIMARY KEY
+-- );
